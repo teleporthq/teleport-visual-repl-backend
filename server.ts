7b7ddc1088da88302b44994d6c4c2527b677134a
Ytrrium <t.cernusca@gmail.com>
2020-02-12 17:26:34 +0200

    Added DB boilerplate

--- server.ts
+++ server.ts
@@ -1,10 +1,30 @@
 const express = require("express");
 const cors = require("cors");
+const mysql = require("mysql");
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
 // CONNECT TO DB
+const dbConnection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "Password4$",
+  database: "teleportVisualReplDb"
+});
+
+dbConnection.connect(function(err) {
+  if (err) {
+    throw err;
+  }
+  console.log("Connected to DB");
+  dbConnection.query("select * from Users", (err, res) => {
+    if (err) {
+      throw err;
+    }
+    console.log(res);
+  });
+});
 
 // MIDDLEWARE
 app.use(cors());
