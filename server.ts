90a66f2a457ddbc13358595990c34c6293f98564
vladgrecu <vladgrecu.smp@gmail.com>
2020-02-13 18:05:52 +0200

    added jwb to api

--- server.ts
+++ server.ts
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const mysql = require("mysql");
@@ -6,25 +7,25 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 // CONNECT TO DB
-const dbConnection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Password4$",
-  database: "teleportVisualReplDb"
-});
+// const dbConnection = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "Password4$",
+//   database: "teleportVisualReplDb"
+// });
 
-dbConnection.connect(function(err) {
-  if (err) {
-    throw err;
-  }
-  console.log("Connected to DB");
-  dbConnection.query("select * from Users", (err, res) => {
-    if (err) {
-      throw err;
-    }
-    console.log(res);
-  });
-});
+// dbConnection.connect(function(err) {
+//   if (err) {
+//     throw err;
+//   }
+//   console.log("Connected to DB");
+//   dbConnection.query("select * from Users", (err, res) => {
+//     if (err) {
+//       throw err;
+//     }
+//     console.log(res);
+//   });
+// });
 
 // MIDDLEWARE
 app.use(cors());
