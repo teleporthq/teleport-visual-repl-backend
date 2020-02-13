9a474ce43de151c5dd207128884192483abf6bfd
vladgrecu <vladgrecu.smp@gmail.com>
2020-02-13 18:05:52 +0200

    added jwb to api

---
+++ controllers/authenticate.ts
@@ -0,0 +1,17 @@
+const jwt = require("jsonwebtoken");
+
+export const authenticate = (req, res, next) => {
+  const authHeader = req.headers["authorization"];
+
+  if (authenticate == null) {
+    return res.sendStatus(403);
+  }
+
+  jwt.verify(authHeader, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) {
+      return res.sendStatus(403);
+    }
+    req.user = user;
+    next();
+  });
+};
--- routes/authorize.ts
+++ routes/authorize.ts
@@ -2,15 +2,39 @@ import * as express from "express";
 const router = express.Router();
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
+const jwt = require("jsonwebtoken");
 
 // placeholder to test out routes till we connect to a db
 
-const fakeDataBase = [
-  { name: "Tudor", password: "123" },
-  { name: "Ale", password: "1234" },
-  { name: "Vlad", password: "111" },
-  { name: "Ionut", password: "florinSalam" }
-];
+const fakeDataBase = [];
+
+router.post("/register", async (req, res) => {
+  const isFound = fakeDataBase.find(user => req.body.name === user.name);
+  if (isFound) {
+    res.status(418).send({
+      error: `User ${req.body.name} already exists! Pick something else!`
+    });
+    return;
+  }
+
+  try {
+    const hashedPassword = await hashPassword(req.body.password);
+    fakeDataBase.push({
+      name: req.body.name,
+      password: hashedPassword,
+      email: req.body.email
+    });
+    const user = { name: req.body.name, email: req.body.email };
+    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+    res.status(201).send({
+      accessToken,
+      message: "Register succesfull!",
+      greet: `Welcome ${req.body.name}`
+    });
+  } catch (error) {
+    res.status(400).send({ error: "Something went wrong" });
+  }
+});
 
 router.post("/signin", async (req, res) => {
   const isFound = fakeDataBase.find(user => {
@@ -26,14 +50,14 @@ router.post("/signin", async (req, res) => {
     req.body.password,
     isFound.password
   );
-  // Check with Postman with a new registered user and after with someone unregistered to see the difference
-  console.log(
-    "User is in the DB!Has he provided the correct password? ",
-    passwordMatch
-  );
 
   if (passwordMatch) {
+    // - generate JWT token for user
+    const user = { name: isFound.name, email: isFound.email };
+    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+
     res.status(200).send({
+      accessToken,
       message: "Sign in succesfull!",
       greet: `Welcome ${req.body.name}`
     });
@@ -43,25 +67,33 @@ router.post("/signin", async (req, res) => {
   res.status(403).send({ error: "Wrong username or password! " });
 });
 
-router.post("/register", (req, res) => {
-  const isFound = fakeDataBase.find(user => req.body.name === user.name);
-  if (isFound) {
-    res.status(418).send({
-      error: `User ${req.body.name} already exists! Pick something else!`
+const hashPassword = async (password): Promise<string> => {
+  return new Promise((resolve, reject) => {
+    bcrypt.hash(password, saltRounds, (err, hash: string) => {
+      err ? reject(err) : resolve(hash);
     });
-  } else {
-    bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
-      try {
-        fakeDataBase.push({ name: req.body.name, password: hash });
-        res.status(201).send({
-          message: "Register succesfull!",
-          greet: `Welcome ${req.body.name}`
-        });
-      } catch (err) {
-        console.log(err);
-      }
-    });
-  }
-});
+  });
+};
 
 module.exports = router;
+
+//  FOR FUTURE REFERENCE
+
+// const authorizeUser = (req, res, next) => {
+//   const authJWT = req.headers["authorization"];
+//   if (!authJWT) {
+//     return res.send(403);
+//   }
+
+//   let decrypredJWT = jwt.decrypt(authJWT);
+//   decrypredJWT = {
+//     canReadUIDL: true,
+//     canWRITEUIDL: false
+//   };
+
+//   if (!decrypredJWT.canWriteUIDL) {
+//     return res.send(403);
+//   }
+
+//   next();
+// };
