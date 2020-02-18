8c71faf864508ce05aab3a07a8914c842d2e463c
Ytrrium <t.cernusca@gmail.com>
2020-02-18 14:11:08 +0200

    Changed routes

--- src/routes/uidl.ts
+++ src/routes/uidl.ts
@@ -5,14 +5,10 @@ import { uidlController } from "../controllers/uidlController";
 const router = express.Router();
 
 // GET ALL UIDL NAMES THAT THE USER HAS
-router.get("/all", authorization, uidlController.getAllUIDLNames);
+router.get("/", authorization, uidlController.getAllUIDLNames);
 
 // GET SPECIFIC UIDL FOR USER BY ITS NAME
-router.get(
-  "/one/:entryName",
-  authorization,
-  uidlController.getUIDLByNameAndUser
-);
+router.get("/:entryName", authorization, uidlController.getUIDLByNameAndUser);
 
 // ADD UIDL OR UPDATE EXISTING UIDL FOR THE USER
 router.post("/save", authorization, uidlController.saveUidl);
