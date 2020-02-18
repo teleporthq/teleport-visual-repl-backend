aecdd7aa432dc88b8aba43b79a0f7686de6a6ff3
Ytrrium <t.cernusca@gmail.com>
2020-02-18 16:18:46 +0200

    Increased request payload limit

--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 8080;
 
 // MIDDLEWARE
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
 
 // ROUTES
 app.use("/authentication", authenticationRoute);
